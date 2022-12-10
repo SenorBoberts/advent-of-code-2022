783cce9411b965c84d78ba1bb160ae7941e34fd2
Benjamin Roberts <benjaminrobertsplus1@gmail.com>
2022-12-10 15:11:12 -0500

    hopefully this fixed it

--- day5/target/debug/deps/day5-b49be4bf6fb6736d.d
+++
@@ -1,5 +0,0 @@
-/home/bobert/Documents/GitHub/advent-of-code-2022/day5/target/debug/deps/day5-b49be4bf6fb6736d.rmeta: src/main.rs
-
-/home/bobert/Documents/GitHub/advent-of-code-2022/day5/target/debug/deps/day5-b49be4bf6fb6736d.d: src/main.rs
-
-src/main.rs:
--- day5/target/debug/deps/day5-d18a9557649f013b.d
+++
@@ -1,5 +0,0 @@
-/home/bobert/Documents/GitHub/advent-of-code-2022/day5/target/debug/deps/day5-d18a9557649f013b.rmeta: src/main.rs
-
-/home/bobert/Documents/GitHub/advent-of-code-2022/day5/target/debug/deps/day5-d18a9557649f013b.d: src/main.rs
-
-src/main.rs:
